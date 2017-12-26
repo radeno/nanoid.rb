d0940f69e9af21e63150afd0b3a968734c315b7c
Anton Lee <antoshalee@gmail.com>
2017-12-27 01:04:58 +0300

    Optimize strings concatenation

--- lib/nanoid.rb
+++ lib/nanoid.rb
@@ -14,8 +14,8 @@ module Nanoid
   def self.simple_generate(size:)
     bytes = random_bytes(size)
 
-    (0...size).reduce('') do |acc, i|
-      acc + SAFE_ALPHABET[bytes[i] & 63]
+    (0...size).reduce(''.dup) do |acc, i|
+      acc << SAFE_ALPHABET[bytes[i] & 63]
     end
   end
 
@@ -24,7 +24,7 @@ module Nanoid
     mask = (2 << Math.log(alphabet_size - 1) / Math.log(2)) - 1
     step = (1.6 * mask * size / alphabet_size).ceil
 
-    id = ''
+    id = ''.dup
     catch :computed do
       loop do
         bytes = random_bytes(size)
@@ -32,7 +32,7 @@ module Nanoid
           byte = bytes[i] & mask
           character = byte && alphabet[byte]
           if character
-            id = "#{id}#{character}"
+            id << character
             throw :computed if id.size == size
           end
         end
